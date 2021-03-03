eda06d0ae27e665b17f664cb9bf582430da31da1
ipasti <69072586+ipasti@users.noreply.github.com>
2021-03-04 01:06:40 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" 
  
 client.commands = new Discord.Collection();
 
+let guild = client.guilds.cache.fetch('798913368169513010');
+
 //подтверждение знание правил, а так же консоль >< 
 const channel = "798913368203198488";
-const accept = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "798913368169513017");
-const pc = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "798913368169513016");
-const xbox = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "798913368169513019");
-const ps = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "798913368177508362");
+const accept = await guild.roles.cache.find(role => role.id === "798913368169513017");
+const pc = await guild.roles.cache.find(role => role.id === "798913368169513016");
+const xbox = await guild.roles.cache.find(role => role.id === "798913368169513019");
+const ps = await guild.roles.cache.find(role => role.id === "798913368177508362");
 
 //объявляю id сообщений
 const message1 = "814600686355742728" //подтверждение
@@ -24,18 +26,18 @@ const xboxEmoji = "814022506208952350";
 const acceptEmoji = "✅";
 //Пол/18+/Игры
 const channel1 = "813416099038756884";
-const man = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "798913368169513018");
-const woman = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "798913368177508365");
-const gta = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813769952153567262");
-const cs = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813769953806778368");
-const apex = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813769949264609321");
-const minecraft = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813769944499879986");
-const warzone = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813769941852618772");
-const ets = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813769935967748096");
-const valorant = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813769939101024327");
-const rainbow6 = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813836529189388321");
-const swbattlefront2 = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "813836528459317260");
-const nsfw = client.guilds.cache.get("798913368169513010").guild.roles.cache.find(role => role.id === "799928306568134707");
+const man = await guild.roles.cache.find(role => role.id === "798913368169513018");
+const woman = await guild.roles.cache.find(role => role.id === "798913368177508365");
+const gta = await guild.roles.cache.find(role => role.id === "813769952153567262");
+const cs = await guild.roles.cache.find(role => role.id === "813769953806778368");
+const apex = await guild.roles.cache.find(role => role.id === "813769949264609321");
+const minecraft = await guild.roles.cache.find(role => role.id === "813769944499879986");
+const warzone = await guild.roles.cache.find(role => role.id === "813769941852618772");
+const ets = await guild.roles.cache.find(role => role.id === "813769935967748096");
+const valorant = await guild.roles.cache.find(role => role.id === "813769939101024327");
+const rainbow6 = await guild.roles.cache.find(role => role.id === "813836529189388321");
+const swbattlefront2 = await guild.roles.cache.find(role => role.id === "813836528459317260");
+const nsfw = await guild.roles.cache.find(role => role.id === "799928306568134707");
  
 const nsfwE = "🔞";
 const manEmoji = "👨";
